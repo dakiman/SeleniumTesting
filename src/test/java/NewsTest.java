273609b118d5e7f93571cc208f008c834ffee07b
dakiman <dvancov@hotmail.com>
2018-05-29 19:51:31 +0200

    More news tests added

--- src/test/java/NewsTest.java
+++ src/test/java/NewsTest.java
@@ -1,14 +1,9 @@
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.*;
 import utils.RandomUtils;
-
-import java.util.concurrent.TimeUnit;
 
 class NewsTest extends WebDriverConfig {
 
@@ -44,7 +39,7 @@ class NewsTest extends WebDriverConfig {
     }
 
     @Test
-    void newsShouldGetDeleted() {
+    void newsCanBeEdited() {
         //Navigate to the news creation page
         driver.get(baseUrl + "/admin/news/create");
         //Create the items title first so we can use it to confirm its existence
@@ -73,13 +68,70 @@ class NewsTest extends WebDriverConfig {
         driver.get(baseUrl + "/admin/news");
         Assert.assertTrue(driver.getPageSource().contains(title_mk_value));
 
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        WebElement deleteBtn = driver.findElement(By.xpath("//button[@class='btn btn-delete btn-danger btn-block m-b-1']"));
-        deleteBtn.click();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        WebElement confirmBtn = driver.findElement(By.className("mark-as-resolved-btn"));
-        confirmBtn.click();
-        Assert.assertTrue(!deleteBtn.isDisplayed());
+        WebElement editBtn = driver.findElement(By.xpath("//div[div/text() = '"+title_mk_value+"']/a/button[@class='btn btn-success btn-block m-b-1']"));
+        editBtn.click();
+        Assert.assertTrue(driver.getPageSource().contains(title_mk_value));
+
+        title_en = driver.findElement(By.name("title_en"));
+        description_en = driver.findElement(By.name("description_en"));
+        title_mk = driver.findElement(By.name("title_mk"));
+        description_mk = driver.findElement(By.name("description_mk"));
+
+        title_mk_value = RandomUtils.randomString(10);
+        String description_mk_value = RandomUtils.randomString(10);
+        String title_en_value = RandomUtils.randomString(10);
+        String description_en_value = RandomUtils.randomString(10);
+
+        title_en.sendKeys(title_en_value);
+        description_en.sendKeys(description_en_value);
+        title_mk.sendKeys(title_mk_value);
+        description_mk.sendKeys(description_mk_value);
+        driver.findElement(By.xpath("//button[@class='btn btn-primary btn-block']")).click();
+
+        Assert.assertTrue(driver.getPageSource().contains(title_mk_value));
+        Assert.assertTrue(driver.getPageSource().contains(description_mk_value));
+        Assert.assertTrue(driver.getPageSource().contains(title_en_value));
+        Assert.assertTrue(driver.getPageSource().contains(description_en_value));
     }
 
+    @Test
+    void newsShouldGetDeleted() {
+        //Navigate to the news creation page
+        driver.get(baseUrl + "/admin/news/create");
+        //Create the items title first so we can use it to confirm its existence
+        String title_mk_value = RandomUtils.randomString(10);
+
+        //get the input fields
+        WebElement title_en = driver.findElement(By.name("title_en"));
+        WebElement description_en = driver.findElement(By.name("description_en"));
+        WebElement title_mk = driver.findElement(By.name("title_mk"));
+        WebElement description_mk = driver.findElement(By.name("description_mk"));
+        WebElement upload = driver.findElement(By.name("cat_img"));
+
+        //populate the form
+        title_en.sendKeys(RandomUtils.randomString(10));
+        description_en.sendKeys(RandomUtils.randomString(30));
+        title_mk.sendKeys(title_mk_value);
+        description_mk.sendKeys(RandomUtils.randomString(30));
+        upload.sendKeys(resourceDir + "J9chN5s.jpg");
+
+        //submit the form
+        title_en.submit();
+        //assert success message is present on screen
+        Assert.assertTrue(driver.getPageSource().contains("Успешно додадено."));
+
+        //navigate to index page for news and assert that the item we created exists
+        driver.get(baseUrl + "/admin/news");
+        Assert.assertTrue(driver.getPageSource().contains(title_mk_value));
+        //find and click the delete button of the element we created
+        WebElement deleteBtn = driver.findElement(By.xpath("//div[div/text() = '"+ title_mk_value +"']/button[@class='btn btn-delete btn-danger btn-block m-b-1']"));
+        deleteBtn.click();
+        //wait for modal to appear and click confirm
+        WebElement confirmBtn = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='btn-confirm']")));
+        confirmBtn.click();
+        //check if container for item is visible
+        WebElement box = driver.findElement(By.xpath("//div[div/div/text() = '"+title_mk_value+"']"));
+        Assert.assertFalse(box.isDisplayed());
+    }
 }
